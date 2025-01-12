bf585b8da35bf18f50263b8ddef34641d83c9463
Martin Hooper <martin@mjhooper.co.uk>
2025-01-12 10:38:21 +0000

    .

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 			fontFamily:{
-				/* robotoslab:["Roboto Slab",'serif'] */
+				/* creates new font classes to use - font-alex and font-aleg  Still wont apply for some reason*/
 				aleg:['Alegreya','serif'],
 				alex:['Alex Brush','cursive']
 			}
